6e6165268952fcc0b66d3dcd22bad42a7df25632
paulohdiniz <ph.diniz@usp.br>
2020-04-01 15:55:30 -0300

    passando contatos e usuarios ok

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const fs = require('fs');
-const caminho = path.join('db','newsletter.json');
-
+const caminhoInscritos = path.join('db','newsletter.json');
+const caminhoContatos = path.join('db', 'contato.json');
 const adminController = {
   
   index: (req, res, next) => {
-    let inscritos = fs.readFileSync(caminho);
-    console.log(inscritos, { encoding: 'utf-8'});
+    let inscritos = fs.readFileSync(caminhoInscritos);
+    let contatos = fs.readFileSync(caminhoContatos);
+    inscritos = JSON.parse(inscritos, { encoding: 'utf-8'});
+    contatos = JSON.parse(contatos, { encoding: 'utf-8'});
+    console.log(contatos);
+    res.render("admin", {title: "admin", inscritos: inscritos.inscritos, contatos: contatos.contatos});
   }
   
   
