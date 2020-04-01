ea58f80c77c821aedfbcdbb521f0f2723e54ff31
Alessandro Menezes <alessandroadm@live.com>
2020-04-01 17:04:19 -0300

    concluido a view e o controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const caminhoInscritos = path.join('db','newsletter.json');
 const caminhoContatos = path.join('db', 'contato.json');
+const caminhoUsuarios = path.join('db', 'usuarios.json');
 const adminController = {
   
   index: (req, res, next) => {
@@ -9,8 +10,42 @@ const adminController = {
     let contatos = fs.readFileSync(caminhoContatos);
     inscritos = JSON.parse(inscritos, { encoding: 'utf-8'});
     contatos = JSON.parse(contatos, { encoding: 'utf-8'});
-    console.log(contatos);
+    console.log(inscritos);
     res.render("admin", {title: "admin", inscritos: inscritos.inscritos, contatos: contatos.contatos});
+  },
+  
+
+  renderCadastro: (req, res) => {
+    res.render('cadastroUsuario', { title: 'Cadastro', msg:null });
+  },
+
+
+  
+  cadastro: (req, res) => {
+      let { nome, email, senha } = req.body;
+
+      let infoUsuario = { nome, email, senha };
+
+      let listaUsuarios = {};
+    if (fs.existsSync(caminhoUsuarios)) {
+        // trazendo conteudo do arquivo em formato JSON
+      listaUsuarios = fs.readFileSync(caminhoUsuarios, { encoding: 'utf-8' });
+        // transformando JSON em obj
+        listaUsuarios = JSON.parse(listaUsuarios);
+        // pegando array de inscritos e adicionando um novo email
+      listaUsuarios.usuarios.push(infoUsuario);
+      } else {
+        listaUsuarios = {
+          usuarios: [infoUsuario]
+        };
+      }
+      // transforma obj em JSON
+      listaUsuarios = JSON.stringify(listaUsuarios);
+      // guardando lista de inscritos com o novo email
+      fs.writeFileSync(caminhoUsuarios, listaUsuarios);
+
+      // -----
+    res.render('cadastroUsuario', { nome, email, senha, title: 'Cadastro', msg:'Cadastro realizado com sucesso!' });
   }
   
   
--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,8 @@ let router = express.Router();
 let adminController = require('../controllers/adminController');
 
 router.get('/', adminController.index);
+router.get('/cadastro', adminController.renderCadastro);
+router.post('/cadastro', adminController.cadastro);
 
 
 module.exports = router;
