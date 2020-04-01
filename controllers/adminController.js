8894fd1967c10ab814ee2cb64296f743a1c002e7
Alessandro Menezes <alessandroadm@live.com>
2020-04-01 17:45:49 -0300

    parte de login

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const caminhoInscritos = path.join('db','newsletter.json');
 const caminhoContatos = path.join('db', 'contato.json');
 const caminhoUsuarios = path.join('db', 'usuarios.json');
+const bcrypt = require('bcrypt');
+
+
 const adminController = {
   
   index: (req, res, next) => {
@@ -14,38 +17,57 @@ const adminController = {
     res.render("admin", {title: "admin", inscritos: inscritos.inscritos, contatos: contatos.contatos});
   },
   
-
+  
   renderCadastro: (req, res) => {
     res.render('cadastroUsuario', { title: 'Cadastro', msg:null });
   },
 
-
+  renderLogin: (req, res) => {
+    res.render('login', { title: 'Login', msg:null });
+  },
+  
+  
   
   cadastro: (req, res) => {
-      let { nome, email, senha } = req.body;
-
-      let infoUsuario = { nome, email, senha };
-
-      let listaUsuarios = {};
+    let { nome, email, senha } = req.body;
+    let senhaCriptografada = bcrypt.hashSync(senha, 10);
+    let infoUsuario = { nome, email, senhaCriptografada };
+    
+    let listaUsuarios = {};
     if (fs.existsSync(caminhoUsuarios)) {
-        // trazendo conteudo do arquivo em formato JSON
+      // trazendo conteudo do arquivo em formato JSON
       listaUsuarios = fs.readFileSync(caminhoUsuarios, { encoding: 'utf-8' });
-        // transformando JSON em obj
-        listaUsuarios = JSON.parse(listaUsuarios);
-        // pegando array de inscritos e adicionando um novo email
+      // transformando JSON em obj
+      listaUsuarios = JSON.parse(listaUsuarios);
+      // pegando array de inscritos e adicionando um novo email
       listaUsuarios.usuarios.push(infoUsuario);
-      } else {
-        listaUsuarios = {
-          usuarios: [infoUsuario]
-        };
-      }
-      // transforma obj em JSON
-      listaUsuarios = JSON.stringify(listaUsuarios);
-      // guardando lista de inscritos com o novo email
-      fs.writeFileSync(caminhoUsuarios, listaUsuarios);
-
-      // -----
+    } else {
+      listaUsuarios = {
+        usuarios: [infoUsuario]
+      };
+    }
+    // transforma obj em JSON
+    listaUsuarios = JSON.stringify(listaUsuarios);
+    // guardando lista de inscritos com o novo email
+    fs.writeFileSync(caminhoUsuarios, listaUsuarios);
+    console.log(infoUsuario);
+    // -----
     res.render('cadastroUsuario', { nome, email, senha, title: 'Cadastro', msg:'Cadastro realizado com sucesso!' });
+  },
+
+  login: (req, res) => {
+    let { senha, email } = req.body;
+
+    let usuarios = fs.readFileSync(caminhoUsuarios, { encoding: 'utf-8' });
+    usuarios = JSON.parse(usuarios);
+
+    let usuarioEncontrado = usuarios.usuarios.filter((usuario) => {
+      return usuario.email == email;
+    });
+
+    console.log(usuarioEncontrado);
+    let comparacao = bcrypt.compareSync(senha, usuarioEncontrado[0].senhaCriptografada);
+    console.log(comparacao);
   }
   
   
--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ let adminController = require('../controllers/adminController');
 router.get('/', adminController.index);
 router.get('/cadastro', adminController.renderCadastro);
 router.post('/cadastro', adminController.cadastro);
+router.post('/login', adminController.login);
+router.get('/login', adminController.renderLogin);
 
 
 module.exports = router;
